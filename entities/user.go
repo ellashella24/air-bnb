a4c10e47a2e233880506efc03769549e883d6405
ellashella24 <ella.shella24@gmail.com>
2022-01-24 21:01:28 +0700

    update entities

--- entities/user.go
+++ entities/user.go
@@ -4,11 +4,11 @@ import "gorm.io/gorm"
 
 type User struct {
 	gorm.Model
-	ID         uint
-	Name       string
-	Email      string
-	Password   string
-	Role       string `gorm:"default:user"`
-	BookingID  []Booking
-	HomestayID []Homestay
+	ID       uint
+	Name     string
+	Email    string
+	Password string
+	Role     string `gorm:"default:user"`
+	Booking  []Booking
+	Homestay []Homestay `gorm:"foreignKey:HostID"`
 }
