4bcfd5c36feaad16015407c32f86a14f9675eec1
Yoga Wahyudi <yogawahyudi7@gmail.com>
2022-01-27 13:21:30 +0700

    unit test controller 87

--- delivery/controllers/homestay/homestay.go
+++ delivery/controllers/homestay/homestay.go
@@ -1,9 +1,11 @@
 package homestay
 
 import (
+	"air-bnb/delivery/common"
 	"air-bnb/delivery/middlewares"
 	"air-bnb/entities"
 	"air-bnb/repository/homestay"
+	"fmt"
 	"net/http"
 
 	"github.com/labstack/echo/v4"
@@ -22,16 +24,10 @@ func (s StructCtrlHomestay) GetAllHomestay() echo.HandlerFunc {
 
 		res, err := s.repository.GetallHomestay()
 		if err != nil {
-			return c.JSON(http.StatusBadRequest, map[string]interface{}{
-				"message": "gagal gaes get all homestay",
-				"data":    err,
-			})
+			return c.JSON(http.StatusBadRequest, common.NewNotFoundResponse())
 		}
 
-		return c.JSON(http.StatusOK, map[string]interface{}{
-			"message": "success get all homestay",
-			"data":    res,
-		})
+		return c.JSON(http.StatusOK, common.SuccessResponse(res))
 	}
 }
 
@@ -41,15 +37,9 @@ func (s StructCtrlHomestay) GetAllHostHomestay() echo.HandlerFunc {
 		userId := middlewares.NewAuth().ExtractTokenUserID(c)
 		res, err := s.repository.GetAllHostHomestay(userId)
 		if err != nil {
-			return c.JSON(http.StatusBadRequest, map[string]interface{}{
-				"message": "gagal gaes get all host homestay",
-				"data":    err,
-			})
+			return c.JSON(http.StatusBadRequest, common.NewNotFoundResponse())
 		}
-		return c.JSON(http.StatusOK, map[string]interface{}{
-			"message": "success get all host homestay",
-			"data":    res,
-		})
+		return c.JSON(http.StatusOK, common.SuccessResponse(res))
 	}
 
 }
@@ -61,9 +51,7 @@ func (s StructCtrlHomestay) CreateHomestay() echo.HandlerFunc {
 		var homestay FormReqCreate
 		err := c.Bind(&homestay)
 		if err != nil {
-			return c.JSON(http.StatusBadRequest, map[string]interface{}{
-				"message": "salah input gaess",
-			})
+			return c.JSON(http.StatusBadRequest, common.ErrorResponse(400, "wrong input"))
 		}
 		var create entities.Homestay
 		create.Name = homestay.Name
@@ -73,15 +61,9 @@ func (s StructCtrlHomestay) CreateHomestay() echo.HandlerFunc {
 
 		res, err2 := s.repository.CreteaHomestay(create)
 		if err2 != nil {
-			return c.JSON(http.StatusBadRequest, map[string]interface{}{
-				"message": "gagal create homestay gas",
-				"data":    err2,
-			})
+			return c.JSON(http.StatusInternalServerError, common.ErrorResponse(500, "failed create homestay"))
 		}
-		return c.JSON(http.StatusOK, map[string]interface{}{
-			"message": "ok mantap create success",
-			"data":    res,
-		})
+		return c.JSON(http.StatusOK, common.SuccessResponse(res))
 	}
 }
 
@@ -92,32 +74,22 @@ func (s StructCtrlHomestay) UpdateHomestay() echo.HandlerFunc {
 
 		fond, err := s.repository.GetHomestayIdByHostId(userId)
 		if err != nil {
-			return c.JSON(http.StatusBadRequest, map[string]interface{}{
-				"message": "gagal gaes menemukan id homestay",
-			})
+			return c.JSON(http.StatusBadRequest, common.ErrorResponse(400, "host ID not found"))
 		}
-
+		fmt.Println(fond)
 		update := FormReqUpdate{}
 		err2 := c.Bind(&update)
 		if err2 != nil {
-			return c.JSON(http.StatusBadRequest, map[string]interface{}{
-				"message": "salah input gaess",
-			})
+			return c.JSON(http.StatusBadRequest, common.ErrorResponse(400, "wrong input"))
 		}
 		fond.Name = update.Name
 		fond.Price = update.Price
 
 		res, err := s.repository.UpdateHomestay(fond)
 		if err != nil {
-			return c.JSON(http.StatusBadRequest, map[string]interface{}{
-				"message": "gagal save update gas",
-				"data":    err,
-			})
+			return c.JSON(http.StatusInternalServerError, common.ErrorResponse(500, "failed update homestay"))
 		}
-		return c.JSON(http.StatusOK, map[string]interface{}{
-			"message": "ok mantap update success",
-			"data":    res,
-		})
+		return c.JSON(http.StatusOK, common.SuccessResponse(res))
 	}
 
 }
--- entities/homestay.go
+++ entities/homestay.go
@@ -5,9 +5,9 @@ import "gorm.io/gorm"
 type Homestay struct {
 	gorm.Model
 	ID             uint
-	Name           string
+	Name           string `gorm:"not null"`
 	Price          int
-	Booking_Status string
+	Booking_Status string `gorm:"default:available"`
 	HostID         uint
 	City_id        uint
 	BookingID      []Booking
